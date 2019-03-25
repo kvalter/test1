062b6b5aba79c95d1e4e940f2b8cfdf0a0df265f
Konstantin Valter <konstantin.valter@gmail.com>
2019-03-25 19:10:08 +0300

    added 2 cycles

--- sample.js
+++ sample.js
@@ -1,56 +1,75 @@
 
-/*
-var leftBorderWidth =1;
-{
-        let second = 2;
-    console.log(second);
-}
+let money = +prompt("Ваш бюджет на месяц?");
+    time = prompt("Введите дату (YYYY-MM-DD)");
 
-
-let something;
-console.log(something);
-
-let persone = {
-    name: "John",
-    age: "25",
-    isMarried: false
-}
-
-console.log(persone.name);
-*/
-
-//let answer = prompt("Are you here?", "Da");
-
-
-//console.log(typeof(answer));
-
-//console.log("arr" + "- object");
-
-var money = prompt("Ваш бюджет на месяц?");
-var time = prompt("Введите дату (YYYY-MM-DD)");
-
-var appData = {
+let appData = {
     budget: money,
     timeData: time,
     expenses: {},
     optionalExpenses: {},
     income: [],
+    timeData: time,
     savings: false
 };
 
-var exp1 = prompt("Введите статью расходов в этом месяце");
-var exp2 = prompt("Во сколько это обойдется?");
-var exp3 = prompt("Введите статью расходов в этом месяце");
-var exp4 = prompt("Во сколько это обойдется?");
 
-appData.expenses.exp1 = exp2;
-appData.expenses.exp3 = exp4;
+// Cycle #1 with WHILE
+// let i = 0;
+// while (i < 2) {
+//     let a = prompt("Введите статью расходов в этом месяце", '');
+//         b = prompt("Во сколько это обойдется?", ''); 
+// if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+//         && a != '' && b != '' && a.length < 50) {
+//         console.log("done");
+//     appData.expenses[a] = b;
+// } 
+// i++;
+// } 
+
+//Cycle #2 with DO
+// let i = 0;
+// do {
+//     let a = prompt("Please input your monthly expenses", '');
+//         b = prompt("How much it will cost?", '');
+// if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+//     && a != '' && b != '' && a.length < 50) {
+//         console.log("done");
+//         appData.expenses[a] = b;
+//     }
+//     i++;
+//     }
+// while (i < 2);
+
+// Cycle #3 with FOR
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите статью расходов в этом месяце", '');
+        b = prompt("Во сколько это обойдется?", ''); 
+    if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+            && a != '' && b != '' && a.length < 50) {
+            console.log("done");
+        appData.expenses[a] = b;
+    } else {
+
+    }
+    
+};
+
+appData.moneyPerDay = appData.budget / 30;
 
 console.log(appData.budget);
 console.log(appData.timeData);
 
-alert(appData.budget / 30);
+alert("Daily budget: " + appData.moneyPerDay);
 
+if (appData.moneyPerDay < 100) {
+    console.log("Minimum level")
+} else if  (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Avarege level");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("High level");
+} else {
+    console.log("Mistake")
+}
 
 
 
